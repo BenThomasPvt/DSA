7ce409e7cb4aef51129f6257a0e3682dc6833721
BenThomasPvt <benthomaspvt@gmail.com>
2025-05-29 13:55:34 +0530

    Add left rotation problem

---
+++ HackerRank/arrays/left_rotation/left_rotation.py
@@ -0,0 +1,15 @@
+def rotateLeft(d, arr):
+    # Rotate the array d times to the left
+    return arr[d:] + arr[:d]
+
+if __name__ == '__main__':
+    first_multiple_input = input("Enter n and d separated by space: ").rstrip().split()
+    n = int(first_multiple_input[0])
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input(f"Enter {n} integers separated by space: ").rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    # Print the result directly to console
+    print(' '.join(map(str, result)))
